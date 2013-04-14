ee5cedb4475b48f2c4f94d2eac39815a1c5c1138
Chris Groce <cgroce@nd.edu>
2013-04-14 18:16:11 -0400

    Fixed the comment half of commentView
    It work!!!!!

--- src/com/kmcginn/airquotes/CommentViewActivity.java
+++ src/com/kmcginn/airquotes/CommentViewActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -27,7 +28,8 @@ import com.parse.SaveCallback;
 
 public class CommentViewActivity extends Activity {
 	Context context= this;
-	ParseObject post;
+	ParseObject mainPost=null;
+	ParseObject updatedCommentHolder;
 	String postID = "0";
 	String postText = "";
 	String comment = "";
@@ -39,9 +41,11 @@ public class CommentViewActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+		StrictMode.setThreadPolicy(policy); 
 		
 		setContentView(R.layout.activity_comment_view);
-    	Log.e("loc", "beginning of oncreate");
 
 		// load in the post's id
 
@@ -52,7 +56,6 @@ public class CommentViewActivity extends Activity {
 		} catch(Exception e1) {
 			Log.e("newAct", "Unable to get objectId from previous activity: " + e1);			
 		}
-    	Log.e("loc", "before query");
 
 		// query to get full post information
         ParseQuery query = new ParseQuery("Message");
@@ -63,7 +66,8 @@ public class CommentViewActivity extends Activity {
 
 				if (e == null){
         			try {
-	        			
+        					mainPost= post;
+
 	        				// get the message text
 	        				postText = post.getString("text").toString();
 	        				Log.e("post", "postText: " + postText);
@@ -89,13 +93,14 @@ public class CommentViewActivity extends Activity {
 	        			        } catch(Exception e2) {
 	        			        	Log.e("gui", "Unable to intialize text boxes: " + e2);
 	        			        }
-	        			
+    			
         			}
         			catch (Exception e1){
             			Log.e("query", "Exception while retrieving post: " + e1.getMessage());
             			Intent intent2= new Intent(context, MainActivity.class);
             	    	startActivity(intent2);
         			}
+        			loadComments();
 				}
 				else	{
 					Toast.makeText(context, "Query failed", Toast.LENGTH_SHORT).show();
@@ -111,127 +116,12 @@ public class CommentViewActivity extends Activity {
         			Intent intent3= new Intent(context, MainActivity.class);
         	    	startActivity(intent3);
         		}*/
-    	Log.e("loc", "after query");
-    	/*
-        // put new info in text boxes
-        try {
-	        TextView postTextBox = (TextView) findViewById(R.id.postText);
-	        postTextBox.setText(postText);
-        } catch (Exception e1){
-        	Log.e("post", "Unable to set post Text in post: "+e1);
-        }
-        try{
-	        TextView dateTextBox = (TextView) findViewById(R.id.dateText);
-	        dateTextBox.setText(date);
-        } catch (Exception e1){
-        	Log.e("post", "Unable to set date Text in post: "+e1);
-        }
-        try{
-	        TextView userTextBox = (TextView) findViewById(R.id.usernameText);
-	        userTextBox.setText(username);
-        } catch (Exception e1){
-        	Log.e("post", "Unable to set username Text in post: "+e1);
-        }
-        try{
-	        TextView altTextBox = (TextView) findViewById(R.id.altText);
-	        altTextBox.setText(Integer.toString(altitude));
-        } catch (Exception e1){
-        	Log.e("post", "Unable to set altitude Text in post: "+e1);
-        } */
-    	Log.e("loc", "after sets");
-
+    	
         //
         //Update Comments
         //
-/*		String comment= "Hello to you too!";
-		
-		// get current user
-		ParseUser currUser= ParseUser.getCurrentUser();
 
-		// upload the comment
-		ParseObject commentHolder= new ParseObject("Comment");
-		commentHolder.put("text", comment);
-		commentHolder.put("user", currUser.getUsername());
-    	Log.e("loc", "after puts" + currUser.getUsername());
-    	try{
-		commentHolder.saveInBackground(new SaveCallback() {
-			public void done(ParseException e) {
-				if(e == null) {
-					// saved successfully
-					Toast.makeText(context, "Comment saved", Toast.LENGTH_SHORT).show();
-				}
-				else {
-				
-					// did not save successfully
-					Toast.makeText(context, "Unable to save comment: " + e.getMessage(), Toast.LENGTH_LONG).show();
-				}
-			}
-		});
-    	} catch(Exception e1){
-    		Log.e("save","Exception while saving comment: "+e1);
-    	}
-    	Log.e("loc", "after save");
-
-		
-		//	update the relationship
-		ParseRelation relation1 = post.getRelation("Comments");
-    	Log.e("loc", "after add");
-
-		relation1.add(commentHolder);
-    	Log.e("loc", "after add");
-*/
-    	
-  		 // get ListView from activity's View
-            ListView listView = (ListView) findViewById(R.id.commentList);
-          // init arraylist adapter
-          listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-          // attach the adapter to the listview
-        try {
-          listView.setAdapter(listAdapter);
-
-    	} catch (Exception e1) {
-    		Log.e("adapter", "Exception while creating listAdaptor: "+e1);
-    	}
-      	  Log.e("loc", "after adapter");
-      	  
-      	  try {
-          // setup parse query (for comments)
-          ParseRelation relation = post.getRelation("Comments");
-          ParseQuery commentQuery = relation.getQuery();
-          
-          Log.e("loc", "begin query");
-
-          // find them in the background
-          commentQuery.findInBackground(new FindCallback() {
-          	public void done(List<ParseObject> objects, ParseException e) {
-          		if(e == null) {
-          			//success
-          			
-          			// add all objects from query to list adapter
-          			for(Object o: objects){
-          				// get the message
-          				String text = ((ParseObject) o).getString("text").toString();
-          				// get the user
-          				String user = ((ParseObject) o).getString("user").toString();
-          				// add a string combining the message and user
-          				listAdapter.add(text + "\n" + user);
-          			}
-          			
-          			//indicate that the list adapter has changed its data, so the listview will update
-          			listAdapter.notifyDataSetChanged();
-          		}
-          		else {
-          			//failure
-          			Toast.makeText(context, "Unable to retrieve comments", Toast.LENGTH_LONG).show();
-          		}
-          	}
-          });
-	 } catch(Exception e1){
- 		  Log.e("query", "Exception in querying relation: "+e1);
- 	  }
-     
-      	Log.e("loc", "end query");
-
+  		 
     
 	}
 	@Override
@@ -250,8 +140,8 @@ public class CommentViewActivity extends Activity {
 		
 		altitude+=1;
 		
-		post.increment("altitude");
-		post.saveInBackground(new SaveCallback() {
+		mainPost.increment("altitude");
+		mainPost.saveInBackground(new SaveCallback() {
 			public void done(ParseException e) {
 				if(e == null) {
 					// saved successfully
@@ -273,8 +163,8 @@ public class CommentViewActivity extends Activity {
 	public void onDiveClicked(View view){
 				
 		altitude -= 1;
-		post.increment("altitude", -1);
-		post.saveInBackground(new SaveCallback() {
+		mainPost.increment("altitude", -1);
+		mainPost.saveInBackground(new SaveCallback() {
 			public void done(ParseException e) {
 				if(e == null) {
 					// saved successfully
@@ -296,7 +186,7 @@ public class CommentViewActivity extends Activity {
 		// get comment from EditText
 		EditText commentTextBox= (EditText) findViewById(R.id.CommentEdit);
 		String comment= commentTextBox.getText().toString();
-      	Log.e("loc", "start funct");
+		commentTextBox.setText("");
 
 		// get current user
 		ParseUser currUser= ParseUser.getCurrentUser();
@@ -305,26 +195,86 @@ public class CommentViewActivity extends Activity {
 		ParseObject commentHolder= new ParseObject("Comment");
 		commentHolder.put("text", comment);
 		commentHolder.put("user", currUser.getUsername());
-      	Log.e("loc", "funct puts");
-		//	update the relationship
-		ParseRelation relation = post.getRelation("Comments");
-		Log.e("loc","between the two");
-		relation.add(commentHolder);
-      	Log.e("loc", "addededed");
+		try {
+			commentHolder.save();
+			// saved successfully
+			Toast.makeText(context, "Comment saved", Toast.LENGTH_SHORT).show();
+		} catch(Exception e1){
+			// did not save successfully
+			Toast.makeText(context, "Unable to save comment: " + e1.toString(), Toast.LENGTH_LONG).show();
+		}
+      	
+      	try {
+			//	update the relationship
+			ParseRelation relation = mainPost.getRelation("Comments");
+			relation.add(commentHolder);
+      	} catch (Exception e1){
+	    	Log.e("relation", "Exception while adding a relation: "+e1);
+	    }
 
-		post.saveInBackground(new SaveCallback() {
+		mainPost.saveInBackground(new SaveCallback() {
 			public void done(ParseException e) {
 				if(e == null) {
 					// saved successfully
-					Toast.makeText(context, "Comment saved", Toast.LENGTH_SHORT).show();
+					Toast.makeText(context, "Relation saved", Toast.LENGTH_SHORT).show();
 				}
 				else {
 				
 					// did not save successfully
-					Toast.makeText(context, "Unable to save comment: " + e.getMessage(), Toast.LENGTH_LONG).show();
+					Toast.makeText(context, "Unable to save relation: " + e.getMessage(), Toast.LENGTH_LONG).show();
 				}
 			}
 		});
+		
+		loadComments();
+	}
+	public void loadComments(){
+		// get ListView from activity's View
+        ListView listView = (ListView) findViewById(R.id.commentList);
+      // init arraylist adapter
+      listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+      // attach the adapter to the listview
+    try {
+      listView.setAdapter(listAdapter);
+
+	} catch (Exception e1) {
+		Log.e("adapter", "Exception while creating listAdaptor: "+e1);
+	}
+  	  
+  	try {
+        // setup parse query (for comments)
+        ParseRelation relation = mainPost.getRelation("Comments");
+        ParseQuery commentQuery = relation.getQuery();
+        
+        // find them in the background
+        commentQuery.findInBackground(new FindCallback() {
+       	public void done(List<ParseObject> objects, ParseException e) {
+       		if(e == null) {
+       			//success
+          			
+       			// add all objects from query to list adapter
+       			for(Object o: objects){
+       				// get the message
+       				String text = ((ParseObject) o).getString("text").toString();
+       				// get the user
+       				String user = ((ParseObject) o).getString("user").toString();
+       				// add a string combining the message and user
+       				listAdapter.add(text + "\n" + user);
+       			}
+          			
+       			//indicate that the list adapter has changed its data, so the listview will update
+       			listAdapter.notifyDataSetChanged();
+       		}
+       		else {
+       			//failure
+       			Toast.makeText(context, "Unable to retrieve comments", Toast.LENGTH_LONG).show();
+       		}
+       	}
+       	});
+        } catch(Exception e1){
+		  Log.e("query", "Exception in querying relation: "+e1);
+		 }
+ 
 
 	}
 	
